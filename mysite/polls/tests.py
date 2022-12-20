6b85da8deda61361bb9652d915a385b2da027a7a
James Zhu <zhujian@leetcode.cn>
2022-12-20 18:27:03 +0800

    Modify test cases

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,23 +1,73 @@
 from django.test import TestCase
 import pytest
-from .models import Question
+from .models import User, StudyPlan
 from django.utils import timezone
 
-# Create your tests here.
 @pytest.mark.django_db
-class TestQuestionManagerGetById:
+class TestStudyPlanManager:
+    def test_init_data(self):
+        # create data fixtures
+        user_1 = User.objects.create(
+            name="user_1",
+        )
+        user_2 = User.objects.create(
+            name="user_2",
+        )
+        user_3 = User.objects.create(
+            name="user_3",
+        )
+        study_plan_1 = StudyPlan.objects.create(
+            name="study_plan_1",
+        )
+        study_plan_1.users.add(user_1)
+        study_plan_1.users.add(user_2)
+        study_plan_2 = StudyPlan.objects.create(
+            name="study_plan_2",
+        )
+        study_plan_2.users.add(user_2)
+        study_plan_2.users.add(user_3)
+        # save to database
+        user_1.refresh_from_db()
+        user_2.refresh_from_db()
+        user_3.refresh_from_db()
+        study_plan_1.refresh_from_db()
+        study_plan_2.refresh_from_db()
+        # asserts
+        assert user_1.id == 1
+        assert user_2.id == 2
+        assert user_3.id == 3
+        assert study_plan_1.id == 1
+        assert study_plan_2.id == 2
 
-    # We make integration tests for the manager to validate database behaviours
-    def test_get_question_by_id(self):
-        question_1 = Question.objects.create(
-            question_text="question 1", pub_date=timezone.now())
-        question_2 = Question.objects.create(
-            question_text="question 2", pub_date=timezone.now())
+    def test_get_study_plan(self):
+        # create data fixtures
+        user_1 = User.objects.create(
+            name="user_1",
+        )
+        user_2 = User.objects.create(
+            name="user_2",
+        )
+        user_3 = User.objects.create(
+            name="user_3",
+        )
+        study_plan_1 = StudyPlan.objects.create(
+            name="study_plan_1",
+        )
+        study_plan_1.users.add(user_1)
+        study_plan_1.users.add(user_2)
+        study_plan_2 = StudyPlan.objects.create(
+            name="study_plan_2",
+        )
+        study_plan_2.users.add(user_2)
+        study_plan_2.users.add(user_3)
+        # save to database
+        user_1.refresh_from_db()
+        user_2.refresh_from_db()
+        user_3.refresh_from_db()
+        study_plan_1.refresh_from_db()
+        study_plan_2.refresh_from_db()
+        # fetch data
+        study_plan = StudyPlan.objects.get_by_id(1)
+        assert study_plan.id == 1
+        assert study_plan.name == "study_plan_1"
 
-        question = Question.objects.get_by_id(Question, 1)
-
-        question_1.refresh_from_db()
-        question_2.refresh_from_db()
-        assert question_1.id == 1
-        assert question_2.id == 2
-        assert question.id == 1
